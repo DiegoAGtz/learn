1bfa0bd958c9c2cd8913b9b8b325cb6f5daa09f9
DiegoAGtz <diegogtz1207@gmail.com>
2021-06-23 22:25:55 -0500

    Prueba con funciones

--- exercises_level_1/exercise_2.go
+++ exercises_level_1/exercise_2.go
@@ -8,4 +8,11 @@ var z bool
 
 func main() {
     fmt.Println(x, y, z)
+    fmt.Println("")
+    fmt.Println(holaMundo("Mensaje pasado como parametro"))
+}
+
+func holaMundo(mensaje string) string {
+    s1 := fmt.Sprintf("Hola mundo desde una funcion con el mensaje: %s", mensaje)
+    return s1 
 }

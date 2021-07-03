507e08599a055ec2ff7468b8c5d3c4372f0230f0
DiegoAGtz <diegogtz1207@gmail.com>
2021-07-03 15:50:36 -0500

    Se agrego go.mod para dar soporte a librerias de 3ros y nuevos ejercicios

---
+++ experiments/exercise-slices.go
@@ -0,0 +1,20 @@
+package main
+
+// Importar esta libreria con go get
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	res := [][]uint8{}
+	for i := 0; i < dy; i++ {
+		ren := []uint8{}
+		for j := 0; j < dx; j++ {
+			ren = append(ren, uint8((j ^ i)))
+		}
+		res = append(res, ren)
+	}
+	return res
+}
+
+func main() {
+	pic.Show(Pic)
+}
---
+++ experiments/fibonacci_closure.go
@@ -0,0 +1,28 @@
+/*
+	Los closures o "cierres" son implementaciones de las funciones anónimas.
+	Debido a esta propiedad, es posible implementar las funciones como
+	una única instancia.
+	En este ejemplo, calculamos los primeros 10 elementos de la serie de fibonacci.
+*/
+package main
+
+import "fmt"
+
+// fibonacci es una función que retorna una función que retorna un entero
+func fibonacci() func() int {
+	fib1, fib2 := 0, 1
+	return func() int {
+		fib := fib1
+		fib1, fib2 = fib2, fib1 + fib2
+		return fib
+	}
+}
+
+func main() {
+	// f es nuestra "instancia", va guardando el estado
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%v ", f())
+	}
+	fmt.Println()
+}
